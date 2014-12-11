ccee5056dd30c5e5c91f08459236c4c2a0bfc6be
Ron Rodenberg <rodenberg@gmail.com>
2014-12-11 14:05:23 -0600

    added default template

--- ronweb/settings.py
+++ ronweb/settings.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+BASE_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__))
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -64,6 +66,15 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
 
 LANGUAGE_CODE = 'en-us'
 
--- ronweb/urls.py
+++ ronweb/urls.py
@@ -1,12 +1,20 @@
-from django.conf.urls import patterns, include, url
+# from django.conf.urls import patterns, include, url
+#
+# from django.contrib import admin
+# admin.autodiscover()
+#
+# urlpatterns = patterns(
+#     #'',
+#     url(r'^$', 'ronweb.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+#
+#     url(r'^admin/', include(admin.site.urls)),
+# )
 
-from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls import url
+from views import home
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ronweb.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^$', home, name='home'),
+]
